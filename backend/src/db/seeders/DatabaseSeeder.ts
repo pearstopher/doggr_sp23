cce0325564b230c724bca08a7085edc6dfaf6fce
Pearstopher <pearstopher@gmail.com>
2023-04-26 12:14:18 -0700

    added seeder files

--- backend/src/db/seeders/DatabaseSeeder.ts
+++ backend/src/db/seeders/DatabaseSeeder.ts
@@ -1,8 +1,13 @@
 import type { EntityManager } from '@mikro-orm/core';
 import { Seeder } from '@mikro-orm/seeder';
+import {UserSeeder} from "./UserSeeder.js";
+
 
 export class DatabaseSeeder extends Seeder {
 
-  async run(em: EntityManager): Promise<void> {}
-
+	async run(em: EntityManager): Promise<void> {
+		return this.call(em, [
+			UserSeeder
+		]);
+	}
 }
