b06ec103f3350a7c6e7ca1c93f4b5807d722fa5b
Pearstopher <pearstopher@gmail.com>
2023-04-26 12:52:49 -0700

    added users post to routes.ts

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -19,6 +19,41 @@ async function DoggrRoutes(app: FastifyInstance, _options = {}) {
 	app.get("/dbTest", async (req, _reply) => {
 		return req.em.find(User, {});
 	});
+
+
+
+	app.post<{
+		Body: {
+			name: string,
+			email: string,
+			petType: string,
+		},
+		Reply: {
+			message: string,
+		}
+	}>("/users", async (req, reply: FastifyReply) => {
+		// Fish data out of request (auto converts from json)
+		const {name, email, petType} = req.body;
+
+		try {
+			// Get our manager from the plugin we wrote
+			const newUser = await req.em.create(User, {
+				name,
+				email,
+				petType
+			});
+
+			// This will immediately update the real database.  You can store up several changes and flush only once
+			// NOTE THE AWAIT -- do not forget it or weirdness abounds
+			await req.em.flush();
+
+			console.log("Created new user:", newUser);
+			return reply.send(newUser);
+		} catch (err) {
+			console.log("Failed to create new user: ", err.message);
+			return reply.status(500).send({ message: err.message});
+		}
+	});
 }
 
 export default DoggrRoutes;
