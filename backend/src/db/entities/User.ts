997abb50868d4bde60af8c6f965d111cd80d0b87
Pearstopher <pearstopher@gmail.com>
2023-04-26 22:19:32 -0700

    fixed duplicates in user.ts

--- backend/src/db/entities/User.ts
+++ backend/src/db/entities/User.ts
@@ -28,24 +28,4 @@ export class User extends BaseEntity {
 		{cascade: [Cascade.PERSIST, Cascade.REMOVE]}
 	)
 	matched_by!: Collection<Match>;
-
-	@Property()
-	name: string;
-
-	@Property()
-	petType: string;
-
-	// Note that these DO NOT EXIST in the database itself!
-	@OneToMany(
-		() => Match,
-		match => match.owner,
-		{cascade: [Cascade.PERSIST, Cascade.REMOVE]})
-	matches!: Collection<Match>;
-
-	@OneToMany(
-		() => Match,
-		match => match.matchee,
-		{cascade: [Cascade.PERSIST, Cascade.REMOVE]})
-	matched_by!: Collection<Match>;
-
 }
