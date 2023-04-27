fa3848730774ffaec77a8769bca9334473fb7949
Pearstopher <pearstopher@gmail.com>
2023-04-26 22:51:32 -0700

    message no longer inherits from baseentity

--- backend/src/db/entities/Message.ts
+++ backend/src/db/entities/Message.ts
@@ -1,5 +1,5 @@
 
-import { Entity, Property, Unique, ManyToOne } from "@mikro-orm/core";
+import {Entity, Property, Unique, ManyToOne, PrimaryKey} from "@mikro-orm/core";
 import { BaseEntity } from "./BaseEntity.js";
 import { User } from "./User.js";
 
@@ -7,14 +7,17 @@ import { User } from "./User.js";
 // going to extend the base entity unlike match
 // so that I can have a unique ID primary key
 @Entity()
-export class Message extends BaseEntity {
+export class Message {
+
+    @PrimaryKey()
+    id!: number;
 
     // The person who sent the message
-    @ManyToOne({primary: true})
+    @ManyToOne()
     from!: User;
 
     // The person who received the message
-    @ManyToOne({primary: true})
+    @ManyToOne()
     to!: User;
 
     // The body of the message
