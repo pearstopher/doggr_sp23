ee3ebd0e73e4a5027feab6cf469535e2174e58e2
Pearstopher <pearstopher@gmail.com>
2023-04-27 14:40:57 -0700

    some messages are seeded now

---
+++ backend/src/db/seeders/MessageSeeder.ts
@@ -0,0 +1,29 @@
+import type { EntityManager } from '@mikro-orm/core';
+import { Seeder } from '@mikro-orm/seeder';
+import {Message} from "../entities/Message.js";
+import {User} from "../entities/User.js";
+
+export class MessageSeeder extends Seeder {
+	async run(em: EntityManager): Promise<void> {
+		const users = await em.find(User, {});
+		em.create(Message, {
+			from: users[0],
+			to: users[1],
+			body: "This is a message."
+		});
+
+		em.create(Message, {
+			from: users[0],
+			to: users[2],
+			body: "This is a message."
+		});
+
+		em.create(Message, {
+			from: users[0],
+			to: users[3],
+			body: "This is a message."
+		});
+
+
+	}
+}
