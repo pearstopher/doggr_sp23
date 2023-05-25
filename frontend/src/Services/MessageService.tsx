84feecfe87095a8424fe94ad0e1047329557a753
Pearstopher <pearstopher@gmail.com>
2023-05-25 12:58:25 -0700

    added messages field to message service

--- frontend/src/Services/MessageService.tsx
+++ frontend/src/Services/MessageService.tsx
@@ -1,7 +1,7 @@
 import { httpClient } from "@/Services/HttpClient.tsx";
 
 export const MessageService = {
-	async send(sender_id: number, receiver_id: number) {
-		return httpClient.post("/messages", { sender_id, passee_id: receiver_id });
+	async send(sender_id: number, receiver_id: number, message: string) {
+		return httpClient.post("/messages", { sender_id, receiver_id, message });
 	},
 };
